bcd3478d2a234e712d4fdf7e6fa988c69c10a156
Егор Королев <Tyuiu.KorolevES@outlook.com>
2024-10-14 19:10:45 +0500

    Task6

---
+++ Tyuiu.KorolevES.Sprint2.Task6.V2/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.KorolevES.Sprint2.Task6.V2.Lib;
+namespace Tyuiu.KorolevES.Sprint2.Task6.V2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #2 | Выполнил: Королев Е. С.| АСОиУб-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Cпринт #2                                                               *");
+            Console.WriteLine("* Тема: Оператор switch                                                   *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #2                                                              *");
+            Console.WriteLine("* Выполнил: Королев Егор Сергеевич | АСОиУб-24-1                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая использует сокращенную форму записи         *");
+            Console.WriteLine("* оператора switch вычисляет требуемое значение и возвращает результат.   *");
+            Console.WriteLine("* Условие: По данному месяцу, определите в какую пору года попадает этот  *");
+            Console.WriteLine("*  месяц (Зима, Лето, Весна, Осень).                                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Введите номер месяца:                                                   *");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            DataService ds = new DataService();
+            Console.WriteLine(ds.FindMonthSeason(n));
+            Console.ReadKey();
+        }
+    }
+}
